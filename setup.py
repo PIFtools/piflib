e7db24076b9d3bbc2acf9de6dd56514d411f4d23
Wilko Henecka <wilko.henecka@data61.csiro.au>
2021-03-10 12:45:21 +1100

    preparing 0.1.0 release

--- setup.py
+++ setup.py
@@ -10,15 +10,22 @@ requirements = [
 
 setuptools.setup(
     name="piflib",
-    version="0.0.1",
+    version="0.1.0",
     author="CSIRO's DATA61",
     author_email="confidential.computing@data61.csiro.au",
     description="A library for computing the personal information factor (PIF)",
     long_description=readme,
     long_description_content_type='text/markdown',
+    url='https:github.com/PIFtools/piflib',
     license='Apache',
     install_requires=requirements,
     packages=setuptools.find_packages(),
+    project_urls={
+        'Documentation': 'http://piflib.readthedocs.io/',
+        'Source': 'https://github.com/PIFtools/piflib',
+        'Tracker': 'https://github.com/PIFtools/piflib/issues',
+        'Discussion': 'https://github.com/PIFtools/piflib/discussions'
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
