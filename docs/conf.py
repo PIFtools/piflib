589a8bbf014b1f0bde3ea5f1be34729e41e5a72f
Wilko Henecka <wilko.henecka@data61.csiro.au>
2021-03-11 15:10:05 +1100

    oops. broke the docs build

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-import piflib
 sys.path.insert(0, os.path.abspath('..'))
+import piflib
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # -- Project information -----------------------------------------------------
